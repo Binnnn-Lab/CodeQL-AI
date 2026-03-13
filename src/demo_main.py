ec609211cb5269563b5d21c21cfee6cdc7b29d5e
zr4in <54037744+zr4in@users.noreply.github.com>
2026-03-13 11:08:13 +0800

    增加示例代码用于初始化项目结构 (#7)
    * ignore .vscode/
    
    * add template files for src/libs and src/mcptools
    
    * update readme
    
    * add scripts/demo_script.py
    
    * add test/demo_test.py

---
+++ src/demo_main.py
@@ -0,0 +1,11 @@
+from libs import lib_some_lib
+from mcptools import some_tool
+
+
+if __name__=="__main__":
+    print(
+        lib_some_lib("hello")
+    )
+    print(
+        some_tool("hello CodeQL-AI")
+    )
---
+++ src/libs/demo_lib.py
@@ -0,0 +1,10 @@
+def lib_some_lib(arg: str) -> str:
+    """hanlde a string input
+
+    Args:
+        arg (str): input arg, a str
+
+    Returns:
+        str: another str
+    """
+    return f"lib_some_lib({arg})"
