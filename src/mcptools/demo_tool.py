dd685ef8ca2828f1696fd9c8afbcc69a44b780bc
Just1cep4rtn3r <Just1cep4rtn3r@gmail.com>
2026-05-23 15:45:15 +0800

    update

---
+++ src/mcptools/demo_tool.py
@@ -0,0 +1,10 @@
+def some_tool(input: str="default"):
+    """a tool for AI
+
+    Args:
+        input (str, optional): input string. Defaults to "default".
+
+    Returns:
+        _type_: another string.
+    """
+    return f"Input is {input}"
