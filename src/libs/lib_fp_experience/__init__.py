d18ee8b2f7ed0223a07da98d05b2ffc58adf0ad4
zr4in <zr4in@qq.com>
2026-05-27 16:54:37 +0800

    添加经验保存与使用的lib

---
+++ src/libs/lib_fp_experience/__init__.py
@@ -0,0 +1,4 @@
+from .lib_fp_extract import analyze_fp_report, read_source_context, save_fp_experience
+from .lib_fp_apply import match_experience_to_ql
+
+__all__ = ["analyze_fp_report", "read_source_context", "save_fp_experience", "match_experience_to_ql"]
